52fa29389506e7a8671236a8fd24690f761da1e8
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-08-08 15:33:31 -0400

    Use a simpler precise32 vagrant box

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,14 +1,16 @@
+Exec { 
+  path => ["/bin/", "/sbin/", "/usr/bin/", "/usr/sbin/"],
+}
+
 package { ["make", "curl", "git", "ruby"]:
   ensure => present,
 }
 
 exec { "rvm":
-  path => ["/usr/bin","/bin"],
   command => "curl -sSL https://get.rvm.io | bash -s stable --ruby=1.9.3 && source ~/.rvm/scripts/rvm",
   require => Package["curl"],
 }
 
 exec { "gem install bundler":
-  path => ["/usr/bin","/bin"],
   require => Exec["rvm"],
 }
