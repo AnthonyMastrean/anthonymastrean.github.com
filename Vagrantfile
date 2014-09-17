830eebe3d1c440a5ae6c2dbb46b3b7a4c6392365
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-09-17 17:44:57 -0400

    Get that vagrant preview working. Fixes #17

--- Vagrantfile
+++ Vagrantfile
@@ -3,6 +3,6 @@ VAGRANTFILE_API_VERSION = "2"
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "hashicorp/precise32"
   config.vm.box_url = "https://vagrantcloud.com/hashicorp/precise32/version/1/provider/virtualbox.box"
-
   config.vm.provision "puppet"
+  config.vm.network "forwarded_port", guest: 4000, host: 4000
 end
--- manifests/default.pp
+++ manifests/default.pp
@@ -2,15 +2,35 @@ Exec {
   path => ["/bin/", "/sbin/", "/usr/bin/", "/usr/sbin/"],
 }
 
-package { ["make", "curl", "git", "ruby"]:
-  ensure => present,
+exec { "apt-get update":
 }
 
-exec { "rvm":
-  command => "curl -sSL https://get.rvm.io | bash -s stable --ruby=1.9.3 && source ~/.rvm/scripts/rvm",
+package { "curl":
+  require => Exec["apt-get update"],
+}
+
+exec { "get rvm":
+  command => "curl -sSL https://get.rvm.io | bash -s stable --ruby=1.9.3",
   require => Package["curl"],
 }
 
+exec { "source rvm":
+  command => "source ~/.rvm/scripts/rvm",
+  require => Exec["get rvm"],
+}
+
 exec { "gem install bundler":
-  require => Exec["rvm"],
+  require => Exec["source rvm"],
+}
+
+exec { "pushd /vagrant":
+  command => "bash -c 'pushd /vagrant'",
+}
+
+exec { "bundle install":
+  require => Exec["gem install bundler", "pushd /vagrant"],
+}
+
+exec { "bundle exec rake generate preview":
+  require => Exec["bundle install"],
 }
